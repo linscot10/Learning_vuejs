dc17038f16ed67474aeec5035c899f12e8d24aaf
linscot10 <linscot10@gmail.com>
2025-06-13 15:15:28 +0300

    accordion

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import TodoList from './components/TodoList.vue';
 import RandomQuote from './components/RandomQuote.vue'
+import Calculator from './components/Calculator.vue'
+import Accordion from './components/Accordion.vue'
 // import ToList from "./components/ToList.vue"
 </script>
 <template>
     <!-- <TodoList /> -->
     <!-- <ToList/> -->
-     <RandomQuote/>
+     <!-- <RandomQuote/> -->
+<!-- <Calculator/> -->
+ <Accordion/>
+
 </template>
 
 <style scoped></style>
---
+++ vue/src/components/Accordion.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+
+const items = ref([
+    {
+        title: "Section 1",
+        content: "Content for section 1 goes here.",
+        open: false
+    },
+    {
+        title: "Section 2",
+        content: "Content for section 2 goes here.",
+        open: false
+    },
+    {
+        title: "Section 3",
+        content: "Content for section 3 goes here.",
+        open: false
+    }])
+
+const toggleAccordion = (index) => {
+    items.value = items.value.map((item, i) => ({
+        ...item,
+        open: i === index ? !item.open : false
+    }))
+}
+</script>
+
+<template>
+    <div class='accordion-container'>
+        <div v-for="(item, index) in items" :key="index" class='accordion'>
+            <div class='accordion-header' @click="toggleAccordion(index)">
+                {{ item.title }}
+                <span>{{ item.open?'🔽':'🔽' }}</span>
+            </div>
+            <div class="accordion-content" v-show="item.open">{{ item.content }}</div>
+        </div>
+    </div>
+</template>
